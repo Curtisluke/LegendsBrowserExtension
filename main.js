1a9061dba353250e4a3fa98b114e613dbb291769
Curtisluke <curtisluke02@gmail.com>
2022-10-12 16:51:50 -0400

    Recognizes if on legends or not

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-alert("On Wookiepedia")
+//console.log(document.URL + "/Legends")
+var URL = document.URL
+if( URL.lastIndexOf("/Legends") != -1) { 
+	console.log("*******************Is Legends*******************")
+}
