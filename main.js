926efaab0cf4d32b46be9ab4a77f554c766c6507
Curtisluke <curtisluke02@gmail.com>
2022-10-12 16:56:38 -0400

    Redirects to legends page with bugs

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 //console.log(document.URL + "/Legends")
 var URL = document.URL
-if( URL.lastIndexOf("/Legends") != -1) { 
+if( URL.lastIndexOf("/Legends") == -1) { 
+	console.log("*******************Is Canon*******************")
+	window.location.href = (URL + "/Legends");
+	console.log("*******************Is Now Legends*******************")
+}
+else {
 	console.log("*******************Is Legends*******************")
 }
