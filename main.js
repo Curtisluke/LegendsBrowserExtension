0866cfdce674d7d844852f482055503ce5dc7fea
Curtisluke <curtisluke02@gmail.com>
2022-10-18 21:30:23 -0400

    Optimized If statements

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-//console.log("*******************Working*******************")
 var hasLegends = document.getElementById("canontab-legends_ctlb")
 
-if( document.URL.lastIndexOf("/Legends") != -1) { 
-	console.log("*******************Is Already Legends*******************")	
-}
-else if (hasLegends != null) {
-	//console.log("*******************Has Legends and is not in it*******************")
-	window.location.href = (hasLegends["firstElementChild"]["href"]);
+var legendsURL = (hasLegends["firstElementChild"]["href"])
+
+if(legendsURL != document.URL && hasLegends != null){
+	window.location.href = legendsURL
 } else {
 	console.log("No Legends Page")
 }
-//console.log("*******************Ended*******************")
